d00a209563f748fb9b396e72e2f4ce8a78563e3c
yatharthagoenka <goenkayathartha2002@gmail.com>
2025-02-01 02:28:28 +0900

    add: tf dummy

---
+++ terraform/module.tf
@@ -0,0 +1,28 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "region" {
+  description = "The GCP region"
+  type        = string
+  default     = "us-central1" # Default region
+}
+
+resource "google_secret_manager_secret" "secret" {
+  secret_id = var.secret_id
+
+  replication {
+    auto {}
+  }
+}
+
+variable "secret_id" {
+  description = "The ID of the secret"
+  type        = string
+}
